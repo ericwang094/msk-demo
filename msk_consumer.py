9747c200df20448cc3c06c96f1f30518139314e4
ericw <eric.wang094@gmail.com>
2022-05-16 23:39:04 +1000

    add consumer

---
+++ msk_consumer.py
@@ -0,0 +1,28 @@
+from kafka import KafkaProducer, KafkaConsumer
+import boto3
+import logging as log
+import sys
+
+log.basicConfig(format='$(asctime)s $(name)-12s $(levelname)-8s $(message)s',
+                datefmt="%Y-%m-%d %H:%M:%S",
+                level=log.INFO)
+
+def main(args):
+    msk_cluster_arn = 'arn:aws:kafka:ap-southeast-2:663493158590:cluster/MSKClusterDemo/4218d9df-08ab-4f76-92c1-2b0c2f3d4257-2'
+    topic = "topic1"
+    message = "hello-from-msk-client"
+
+    try:
+        msk = boto3.client("kafka", region_name="ap-southeast-2")
+        response = msk.get_bootstrap_brokers(ClusterArn=msk_cluster_arn)
+        bootstrap_server = response['BootstrapBrokerString']
+
+        consumer = KafkaConsumer(bootstrap_servers=bootstrap_server)
+        consumer.subscribe([topic])
+        for msg in consumer:
+            log.info("consumer record: \n{}".format(msg))
+    except Exception as ex:
+        pass
+
+if __name__ == "__main__":
+    main(sys.argv)
